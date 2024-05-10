d96e06183493342e62723d9529b1d10527c2d21e
Buyoung Chung <sunarol@naver.com>
2024-05-10 20:18:23 +0900

    feat: Create UE4 project

---
+++ first_project/Source/first_project/first_project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "first_project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, first_project, "first_project" );
